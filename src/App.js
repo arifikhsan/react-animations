ec1faef559b6667bfa8ef3abe22f8fde496add1c
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-05-08 14:59:55 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import ComponentOne from "./ComponentOne";
+import ComponentTwo from "./ComponentTwo";
+import ComponentThree from "./ComponentThree";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <ComponentOne />
+      <ComponentTwo />
+      <ComponentThree />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ComponentThree.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function ComponentOne() {
+  return (
+    <div>
+      <h1>Component Three</h1>
+      <p>
+        Deserunt nulla eu tempor voluptate eu consectetur officia ut anim
+        pariatur Lorem sit. Laborum aliqua aliquip id consectetur. Reprehenderit
+        labore esse excepteur anim mollit sit eiusmod in esse velit dolor
+        veniam. Fugiat aliqua sint occaecat enim anim laboris consequat
+        cupidatat sint qui id fugiat. Cillum minim pariatur proident tempor
+        minim excepteur non aliqua cupidatat esse. Cillum pariatur sunt nostrud
+        aliquip proident irure deserunt et commodo cupidatat in. Laborum in ut
+        aute elit ipsum mollit labore reprehenderit aliquip commodo.
+      </p>
+    </div>
+  );
+}
